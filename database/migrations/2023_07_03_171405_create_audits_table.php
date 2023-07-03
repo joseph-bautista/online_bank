759a22b218916b0999b281e5196653079ac2f64f
Joseph Bautista <40680817+joseph-bautista@users.noreply.github.com>
2023-07-04 02:44:37 +0800

    Docker files causing errors. Removed it first and serve the app on default way of serving laravel lumen app. Also added migration files

---
+++ database/migrations/2023_07_03_171405_create_audits_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('audits', function (Blueprint $table) {
+            $table->id();
+            $table->string('table_name');
+            $table->string('action');
+            $table->string('description');
+            $table->foreignId('user_id');
+            $table->timestamps();
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('audits');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->group(['prefix' => 'api/v1'], function() use ($router){
+    $router->group(['prefix' => 'auth'], function() use ($router){
+        // $router->post('/login', ['']);
+    });
+});
